acb9b8fb04a31f15d9ad7c030880b36d85f272ee
Mike Kazantsev <mk.fraggod@gmail.com>
2015-08-16 14:38:09 +0500

    setup: fix package include, use full README

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python2
 
 from setuptools import setup, find_packages
+import os
 
-
-readme_gist = '''
-Script to run PJSUA SIP client connected to a JACK sound server routing audio to
-whatever sound cards and speaker sets.
-
-It picks up calls, plays klaxon on speakers, followed by the announcement made
-in that call. Music plays in-between announcements.
-
-Script controls PJSUA and JACK to make them work to that effect.
-'''.strip()
-
+# Error-handling here is to allow package to be built w/o README included
+pkg_root = os.path.dirname(__file__)
+try: readme = open(os.path.join(pkg_root, 'README.rst')).read()
+except IOError: readme = ''
 
 setup(
 
     name = 'PagingServer',
-    version = '15.8.54',
+    version = '15.8.56',
     author = 'Dan Ryan, Mike Kazantsev',
     author_email = 'dan@seattlemesh.net, mk.fraggod@gmail.com',
     license = 'GPL-2',
@@ -28,7 +22,7 @@ setup(
     url = 'https://github.com/AccelerateNetworks/PagingServer',
 
     description = 'SIP-based Announcement / PA / Paging / Public Address Server system',
-    long_description = readme_gist,
+    long_description = readme,
 
     classifiers = [
         'Development Status :: 4 - Beta',
@@ -44,13 +38,15 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2 :: Only',
-        'Topic :: Communications :: Telephony'
-        'Topic :: Multimedia :: Sound/Audio' ],
+        'Topic :: Communications :: Telephony',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Mixers',
+        'Topic :: Multimedia :: Sound/Audio :: Speech' ],
 
     install_requires = ['JACK-Client'],
     extras_require = {'sentry': ['raven']},
 
-    packages = find_packages(),
+    py_modules=['paging'],
     include_package_data = True,
 
     entry_points = {
