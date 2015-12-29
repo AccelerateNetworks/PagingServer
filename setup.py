a22252c6bce72a8108ce456efd74aa503d12b63b
Mike Kazantsev <mk.fraggod@gmail.com>
2015-12-29 09:10:25 +0500

    paging-jack-out-all.service: better regexp for matching audio hw

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except IOError: readme = ''
 setup(
 
     name = 'PagingServer',
-    version = '15.12.1',
+    version = '15.12.2',
     author = 'Dan Ryan, Mike Kazantsev',
     author_email = 'dan@seattlemesh.net, mk.fraggod@gmail.com',
     license = 'GPL-2',
