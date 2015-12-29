c07c758f3161f0127281b0f8054c14a094c127db
Mike Kazantsev <mk.fraggod@gmail.com>
2015-12-29 10:18:19 +0500

    paging.example.conf: fix some older cli option names in description

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except IOError: readme = ''
 setup(
 
     name = 'PagingServer',
-    version = '15.12.2',
+    version = '15.12.3',
     author = 'Dan Ryan, Mike Kazantsev',
     author_email = 'dan@seattlemesh.net, mk.fraggod@gmail.com',
     license = 'GPL-2',
