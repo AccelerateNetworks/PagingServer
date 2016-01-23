78c22281d2edb749338e38b3df8b18c42125f000
Mike Kazantsev <mk.fraggod@gmail.com>
2016-01-23 22:27:07 +0500

    setup-scripts.install.debian_jessie.from_debs: -jack

--- setup-scripts/install.debian_jessie.from_debs.sh
+++ setup-scripts/install.debian_jessie.from_debs.sh
@@ -28,7 +28,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3D021F1F4C670809
 echo 'deb http://paging-server.ddns.net/ jessie main' >/etc/apt/sources.list.d/paging-server.list
 apt-get update
 
-apt_install --no-install-recommends jackd1 alsa-utils
+apt_install --no-install-recommends pulseaudio pulseaudio-utils alsa-utils
 apt_install paging-server python-systemd
 
 getent passwd paging &>/dev/null\
@@ -45,17 +45,17 @@ echo
 echo "Edit configuration file in: /etc/paging.conf"
 echo "At least domain/user/pass MUST be specified there in the [sip] section."
 echo
-echo "After that, start the service with: systemctl start paging-jack-out-all paging"
-echo "  check status: systemctl status jack@paging paging"
+echo "Then configure pulseaudio and/or music player instances to start."
+echo
+echo "After that, start the service with: systemctl start paging"
+echo "  check status: systemctl status paging"
 echo "  check service log with: journalctl -ab -u paging"
 echo "  continuously 'tail' log with: journalctl -af -u paging"
 echo "  continuously tail all system logs with: journalctl -af"
 echo
 echo "If service has been started and is running successfully,"
-echo " enable it to run on boot with: systemctl enable paging-jack-out-all paging"
-echo
-echo "OrangePi PC Notes: Unmute Audio lineo in alsamixer by scrolling right and pressing m"
-echo "Start service with:  systemctl start paging-jack-out@hw:0 paging"
-echo "Enable service with: systemctl enable paging-jack-out@hw:0 paging"
+echo " enable it to run on boot with: systemctl enable paging"
 echo
 echo --------------------
+
+exit 0
